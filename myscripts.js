cbd86ef8a7e65d8658fa98d19425dd88ea8f1728
Andrew <andrewhoonchang97@gmail.com>
2023-08-05 18:13:28 -0700

    Adding my javascript and html file with the pseudocode I wrote in

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+// pseudocode
+// I will start by creating a choice of either rock, paper, or scissors
+// Then, I will have players select either rock, paper, or scissors 
+// (and it will not be case sensitive)
+// Finally, it will return the outcome of the game, i.e. if they lost, "You lose!"
+// After finishing a game, it will reroll the choice of rock, paper, or scissors.
+// New function of game(), it will play a 5 round game that keeps score and reports
+// a winner or loser at the end.
